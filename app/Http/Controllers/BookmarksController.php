eff6fa5b97a6e6b483a9c52669560cf281a907cd
Tomonori Ueno <t.uepon@gmail.com>
2018-09-09 22:49:37 +0900

    Adding bookmarks is available

--- app/Http/Controllers/BookmarksController.php
+++ app/Http/Controllers/BookmarksController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Bookmark;
 
 class BookmarksController extends Controller
 {
@@ -20,5 +21,16 @@ class BookmarksController extends Controller
             'name' => 'required',
             'url' => 'required'
         ]);
-    }
+    
+        // Create Bookmark
+        $bookmark = new Bookmark;
+        $bookmark->name = $request->input('name');
+        $bookmark->url = $request->input('url');
+        $bookmark->description = $request->input('description');
+        $bookmark->user_id = auth()->user()->id;
+
+        $bookmark->save();
+
+        return redirect('/home')->with('success', 'Bookmark Added');
+    }    
 }
